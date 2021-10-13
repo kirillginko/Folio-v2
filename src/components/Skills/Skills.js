2e35d7bf97856ba821e787afc49dba66b9ede9e7
Kirill Ginko <kirillginko@gmail.com>
2021-10-12 22:23:40 -0400

    added blur effect to text

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,17 +1,31 @@
 import React from "react"
-import "../Design/design.css"
+import "../Skills/skills.css"
 
 function Skills() {
   return (
     <div className="contet-page" data-scroll-section>
+      <div
+        className="list-interests"
+        data-scroll
+        data-scroll-direction="vertical"
+        data-scroll-target="#direction"
+        data-scroll-speed="2"
+        data-scroll-delay=".05"
+      >
+        <h2>
+          My focus is to create unique digital products that bring a
+          user-friendy, design-driven interactive experience to companies and
+          brands.
+        </h2>
+      </div>
       <div className="list-main">
         <ul className="list-main__books">
           <li
             className="list-main__item blur-effect item-0"
             data-scroll
-            data-scroll-delay="0.8"
+            data-scroll-delay="1"
             data-scroll-speed=".05"
-            data-scroll-call="item-1"
+            data-scroll-call="item-0"
           >
             Skills:
           </li>
@@ -86,40 +100,6 @@ function Skills() {
             data-scroll-call="item-8"
           >
             Express
-          </li>
-        </ul>
-      </div>
-      <div class="list-description">
-        <ul>
-          <li class="blur-effect" data-scroll>
-            <sup class="number-description">(00-1)</sup>
-            <span class="text-description">
-              Lorem Ipsum is simply dummy text.
-            </span>
-          </li>
-          <li class="blur-effect" data-scroll>
-            <sup class="number-description">(00-2)</sup>
-            <span class="text-description">
-              Lorem Ipsum is simply dummy text.
-            </span>
-          </li>
-          <li class="blur-effect" data-scroll>
-            <sup class="number-description">(00-3)</sup>
-            <span class="text-description">
-              Lorem Ipsum is simply dummy text.
-            </span>
-          </li>
-          <li class="blur-effect" data-scroll>
-            <sup class="number-description">(00-4)</sup>
-            <span class="text-description">
-              Lorem Ipsum is simply dummy text.
-            </span>
-          </li>
-          <li class="blur-effect" data-scroll>
-            <sup class="number-description">(00-5)</sup>
-            <span class="text-description">
-              Lorem Ipsum is simply dummy text.
-            </span>
           </li>
         </ul>
       </div>

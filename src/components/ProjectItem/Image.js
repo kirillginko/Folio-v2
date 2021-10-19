1ad2bbdbf309b57e9494ede6721c367d0d44795e
Kirill Ginko <kirillginko@gmail.com>
2021-10-18 22:54:27 -0400

    added transition and hover effects to projects

--- src/components/ProjectItem/Image.js
+++ src/components/ProjectItem/Image.js
@@ -1,13 +1,19 @@
 import React from "react"
 
-export default function Image({ url, opacity, parallaxPos }) {
+export default function Image({
+  url,
+  opacity,
+  parallaxPos,
+  scale,
+  rotationPosition,
+}) {
   return (
     <img
       src={url}
       alt=""
       style={{
         opacity,
-        transform: `translate3d(${parallaxPos.x}px,${parallaxPos.y}px,0px)`,
+        transform: `translate3d(${parallaxPos.x}px,${parallaxPos.y}px,0px) rotate(${rotationPosition}deg) scale(${scale})`,
       }}
     />
   )
--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -1,4 +1,5 @@
 import React, { useRef, useReducer } from "react"
+import cn from "classnames"
 import Image from "./Image"
 import Title from "./Title"
 import Hash from "./Hash"
@@ -7,6 +8,9 @@ import animate from "./animate"
 const initialState = {
   opacity: 0,
   parallaxPos: { x: 0, y: -20 },
+  scale: 0.8,
+  rotationPosition: 0,
+  active: false,
 }
 
 function reducer(state, action) {
@@ -17,10 +21,34 @@ function reducer(state, action) {
         opacity: action.payload,
       }
     }
+    case "MOUSE/ENTER": {
+      return {
+        ...state,
+        active: true,
+      }
+    }
+    case "MOUSE/LEAVE": {
+      return {
+        ...state,
+        active: false,
+      }
+    }
     case "MOUSE/COORDINATES": {
       return {
         ...state,
         parallaxPos: action.payload,
+      }
+    }
+    case "CHANGE/ROTATION": {
+      return {
+        ...state,
+        rotationPosition: action.payload,
+      }
+    }
+    case "CHANGE/SCALE": {
+      return {
+        ...state,
+        scale: action.payload,
       }
     }
     default: {
@@ -32,7 +60,7 @@ function reducer(state, action) {
 export default function ProjectItem({ project, itemIndex }) {
   const listItem = useRef(null)
   const [state, dispatch] = useReducer(reducer, initialState)
-  const easeMethod = "linear"
+  const easeMethod = "easeInOutCubic"
 
   const parallax = event => {
     const speed = -5
@@ -55,16 +83,53 @@ export default function ProjectItem({ project, itemIndex }) {
     })
   }
 
+  const handleScale = (initialScale, newScale, duration) => {
+    animate({
+      fromValue: initialScale,
+      toValue: newScale,
+      onUpdate: (newScale, callback) => {
+        dispatch({ type: "CHANGE/SCALE", payload: newScale })
+        callback()
+      },
+      onComplete: () => {},
+      duration: duration,
+      easeMethod: easeMethod,
+    })
+  }
+
+  const handleRotation = (currentRotation, duration) => {
+    // random number between -15 and 15
+    const newRotation =
+      Math.random() * 15 * (Math.round(Math.random()) ? 1 : -1)
+    animate({
+      fromValue: currentRotation,
+      toValue: newRotation,
+      onUpdate: (newRotation, callback) => {
+        dispatch({ type: "CHANGE/ROTATION", payload: newRotation })
+        callback()
+      },
+      onComplete: () => {},
+      duration: duration,
+      easeMethod: easeMethod,
+    })
+  }
+
   const handleMouseEnter = () => {
-    dispatch({ type: "CHANGE/OPACITY", payload: 1 })
-    // handleOpacity(0, 1, 500)
+    // dispatch({ type: "CHANGE/OPACITY", payload: 1 })
+    dispatch({ type: "MOUSE/ENTER" })
+    handleOpacity(0, 1, 500)
+    handleScale(0.8, 1, 500)
+    handleRotation(state.rotationPosition, 500)
 
     listItem.current.addEventListener("mousemove", parallax)
   }
 
   const handleMouseLeave = () => {
     dispatch({ type: "CHANGE/OPACITY", payload: 0 })
-    // handleOpacity(1, 0, 100)
+    handleOpacity(1, 0, 800)
+    handleScale(1, initialState.scale, 500)
+    handleRotation(state.rotationPosition, 500)
+    dispatch({ type: "MOUSE/LEAVE" })
     dispatch({ type: "MOUSE/COORDINATES", payload: initialState.parallaxPos })
 
     listItem.current.removeEventListener("mousemove", parallax)
@@ -80,12 +145,15 @@ export default function ProjectItem({ project, itemIndex }) {
         url={project.url}
         opacity={state.opacity}
         parallaxPos={state.parallaxPos}
+        scale={state.scale}
+        rotationPosition={state.rotationPosition}
       />
 
-      <div className="info__block">
+      <div className={cn("info__block", { "is-active": state.active })}>
         <p className="info__block_header">
           <span>
-            <Hash />0{itemIndex}
+            <Hash />
+            (0{itemIndex + 1})
           </span>
         </p>
         {project.info.map(element => (

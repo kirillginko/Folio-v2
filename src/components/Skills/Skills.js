b4866d5c3a579d853f9d4c67aaebfa1d4ee35108
Kirill Ginko <kirillginko@gmail.com>
2022-05-02 22:27:58 -0400

    added title to skills and added more skills

--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -4,55 +4,89 @@ import styled from "styled-components"
 function Skills() {
   return (
     <Container data-scroll-section>
-      <SkillItem>
-        <A>1</A>React
-      </SkillItem>
-      <SkillItem>
-        <A>2</A>Gatsby
-      </SkillItem>
-      <SkillItem>
-        <A>3</A>Threejs
-      </SkillItem>
-      <SkillItem>
-        <A>4</A>WebGL
-      </SkillItem>
-      <SkillItem>
-        <A>5</A>NextJs
-      </SkillItem>
-      <SkillItem>
-        <A>6</A>Express
-      </SkillItem>
-      <SkillItem>
-        <A>6</A>JavaScript
-      </SkillItem>
-      <SkillItem>
-        <A>7</A>Nodejs
-      </SkillItem>
-      <SkillItem>
-        <A>8</A>CSS
-      </SkillItem>
-      <SkillItem>
-        <A>9</A>HTML
-      </SkillItem>
+      <Title>
+        <H1>Tech I like to use:</H1>
+      </Title>
+      <SkillsList>
+        <SkillItem>
+          <A>1</A>React
+        </SkillItem>
+        <SkillItem>
+          <A>2</A>Gatsby
+        </SkillItem>
+        <SkillItem>
+          <A>3</A>Threejs
+        </SkillItem>
+        <SkillItem>
+          <A>4</A>WebGL
+        </SkillItem>
+        <SkillItem>
+          <A>5</A>NextJs
+        </SkillItem>
+        <SkillItem>
+          <A>6</A>Express
+        </SkillItem>
+        <SkillItem>
+          <A>6</A>Gsap
+        </SkillItem>
+        <SkillItem>
+          <A>6</A>Ruby
+        </SkillItem>
+        <SkillItem>
+          <A>6</A>Firebase
+        </SkillItem>
+        <SkillItem>
+          <A>6</A>P5.js
+        </SkillItem>
+        <SkillItem>
+          <A>7</A>JavaScript
+        </SkillItem>
+        <SkillItem>
+          <A>8</A>Nodejs
+        </SkillItem>
+        <SkillItem>
+          <A>9</A>Framer-Motion
+        </SkillItem>
+        <SkillItem>
+          <A>8</A>CSS
+        </SkillItem>
+        <SkillItem>
+          <A>9</A>HTML
+        </SkillItem>
+      </SkillsList>
     </Container>
   )
 }
 
 const Container = styled.div`
+  margin: 10rem 1.5rem;
+  max-width: 100%;
+`
+const SkillsList = styled.div`
   display: flex;
   flex-wrap: wrap;
   position: relative;
   border: 1px solid red;
-  max-width: 100%;
-  margin: 10rem 1.5rem;
 `
 const SkillItem = styled.h1`
   display: flex;
+  font-family: PangramLight;
+  font-weight: 700;
   justify-content: flex-start;
   flex-direction: row;
-  font-size: 5vw;
+  font-size: 6vw;
   padding-right: 1rem;
   margin-bottom: 1rem;
+`
+const Title = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  padding-bottom: 1rem;
+  border: 1px solid blue;
+`
+const H1 = styled.h1`
+  font-size: 7vw;
+  padding-right: 0.5rem;
 `
 const A = styled.a`
   font-size: 2vw;

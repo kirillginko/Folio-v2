b1cf3cc5e3ee4f2ea6660917bc174fbe3ae2006b
Kirill Ginko <kirillginko@gmail.com>
2022-07-10 17:29:03 -0400

    styled location section

--- src/components/Location/Location.js
+++ src/components/Location/Location.js
@@ -8,17 +8,13 @@ function Location() {
   }, [])
   return (
     <Container data-scroll-section>
-      <TopContent>
+      <Content>
         <H2>Currently</H2>
-      </TopContent>
-      <TopContent style={{ justifyContent: "flex-start" }}>
+      </Content>
+      <Content style={{ justifyContent: "flex-start" }}>
         <H2>Based</H2>
-      </TopContent>
-      <TopContent style={{ justifyContent: "center" }}>
-        <H2>in</H2>
-      </TopContent>
-      <TopContent style={{ justifyContent: "flex-end" }}>
-        <H2>nyc</H2>
+      </Content>
+      <Content>
         <CircleWrapper>
           <Circle id="circle">
             <div className="text">
@@ -26,7 +22,13 @@ function Location() {
             </div>
           </Circle>
         </CircleWrapper>
-      </TopContent>
+        <LowerContent>
+          <H2>In</H2>
+          <LowerText>
+            <H2>NYC</H2>
+          </LowerText>
+        </LowerContent>
+      </Content>
     </Container>
   )
 }
@@ -38,11 +40,11 @@ const Container = styled.div`
   justify-content: start;
   height: 100vh;
   margin: 0rem 1.5rem;
-  margin-top: 20rem;
+  margin-top: 60rem;
   margin-bottom: 10rem;
-  border: 1px solid red;
+  /* border: 1px solid red; */
 `
-const TopContent = styled.div`
+const Content = styled.div`
   position: relative;
   display: Flex;
   flex-direction: row;
@@ -53,34 +55,63 @@ const TopContent = styled.div`
   border: 1px solid red;
   @media (max-width: 1440px) {
     justify-content: center;
+    flex-direction: column;
     margin: 0rem 0rem;
+  }
+`
+const LowerContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: start;
+  width: 100%;
+  @media (max-width: 1440px) {
+    justify-content: row;
+    text-align: center;
+`
+const LowerText = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  text-align: end;
+  width: 100%;
+  /* border: 1px solid lime; */
+  @media (max-width: 1440px) {
+    justify-content: row;
+    text-align: center;
   }
 `
 const H2 = styled.h2`
   font-size: calc(17vmax * 9 / 16);
-  /* font-family: ivypresto-display, serif; */
   position: relative;
   text-transform: uppercase;
   font-weight: 100;
   font-style: bold;
   padding-bottom: 2rem;
   @media (max-width: 1440px) {
-    font-size: calc(15vmax * 9 / 16);
+    font-size: calc(10vmax * 9 / 16);
   }
 `
 const CircleWrapper = styled.div`
   position: relative;
   display: flex;
-  justify-content: center;
+  justify-content: flex-start;
   height: min-content;
-  margin: 0rem 15rem;
-  border: 1px solid blue;
+  width: 100%;
+  @media (min-width: 2040px) {
+    left: 15rem;
+    /* width: 50%; */
+  }
+  @media (max-width: 1440px) {
+    justify-content: center;
+    text-align: center;
+  }
 `
 const Circle = styled.div`
   position: relative;
   top: 0;
   font-family: brother-1816, sans-serif;
-  font-size: calc(1vmax * 9 / 16);
+  font-size: calc(1vmax * 25 / 16);
   font-weight: bold;
   letter-spacing: 0.3em;
   text-transform: uppercase;
@@ -89,6 +120,11 @@ const Circle = styled.div`
   color: #181bcc;
   @media (max-width: 1440px) {
     font-size: calc(2.5vmax * 9 / 16);
+    left: 0;
+  }
+  @media (min-width: 1440px) {
+    font-size: calc(2.5vmax * 9 / 16);
+    left: 15rem;
   }
   /* filter: blur(0.6px); */
   .text:after {
